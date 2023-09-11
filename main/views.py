92a917fa566a193888c5ab6ac10a0e8a83def245
TomoriNow <seanarcher1974@gmail.com>
2023-09-11 15:06:14 +0700

    updated main

--- main/views.py
+++ main/views.py
@@ -7,7 +7,7 @@ def show_main(request):
         'name' : 'Muhammad Sean Arsha Galant',
         'class': 'PBP International Class',
         'description' : "The game’s most dynamic player needs basketball shoes that can keep up with him. Steph Curry basketball shoes are built from super-lightweight, breathable materials, and use our top cushioning like UA Flow and UA HOVR™ to cushion your landings and propel you forward.",
-        'release date' : '11 September 2023',
+        'release_date' : '11 September 2023',
         'amount' : '30'
     }
 
