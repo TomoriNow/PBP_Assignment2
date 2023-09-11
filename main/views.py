fba2cf0e723879ee3394a95566435197e7e7f8e6
TomoriNow <seanarcher1974@gmail.com>
2023-09-11 15:04:51 +0700

    updated main

--- main/views.py
+++ main/views.py
@@ -5,7 +5,10 @@ def show_main(request):
     context = {
         'appName' : 'Curry Under Armour Inventory',
         'name' : 'Muhammad Sean Arsha Galant',
-        'class': 'International Class'
+        'class': 'PBP International Class',
+        'description' : "The game’s most dynamic player needs basketball shoes that can keep up with him. Steph Curry basketball shoes are built from super-lightweight, breathable materials, and use our top cushioning like UA Flow and UA HOVR™ to cushion your landings and propel you forward.",
+        'release date' : '11 September 2023',
+        'amount' : '30'
     }
 
     return render(request, 'main.html', context)
