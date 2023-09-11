88577ec2992b24c4710f9f853144fa8469771d40
TomoriNow <seanarcher1974@gmail.com>
2023-09-11 14:40:42 +0700

    updated repository

--- main/views.py
+++ main/views.py
@@ -3,7 +3,8 @@ from django.shortcuts import render
 # Create your views here.
 def show_main(request):
     context = {
-        'name': 'Muhammad Sean Arsha Galant',
+        'appName' : 'Curry Under Armour Inventory',
+        'name' : 'Muhammad Sean Arsha Galant',
         'class': 'International Class'
     }
 
